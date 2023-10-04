2040c874886519cd4d415ee751539dcfcbce69b3
NucleonGodX <racerpro41@gmail.com>
2023-10-04 08:52:26 +0530

    navbar done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 export default function Header(){
     return(
-        <header className='headmust'>
+        <header className='flex flex-wrap items-center justify-between p-5 bg-black text-cyan-300 text-2xl'>
         <NavLink to='/' className='car-head'>#YourFourms</NavLink>
         <hr/>
-        <NavLink to="/about" className={({isActive})=>isActive?"background-black": null}>About</NavLink>
+        <div className="flex space-x-8">
+        <NavLink to="/about" className={({isActive})=>isActive?" underline text-yellow-300": null}>About</NavLink>
         <hr/>
-        <NavLink to= "/Login" className={({isActive})=>isActive?"background-black": null}>Login</NavLink>
+        <NavLink to= "/Login" className={({isActive})=>isActive?" underline text-yellow-300": null}>Login</NavLink>
         <hr/>
-        <NavLink to="/Sign-up" className={({isActive})=>isActive?"background-black": null}>Sign up</NavLink>
-    
+        <NavLink to="/Sign-up" className={({isActive})=>isActive?"underline text-yellow-300": null}>Sign up</NavLink>
+        </div>
         </header>
     )
 }
