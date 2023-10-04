20bf463dfa28fc5d65ef7b75f0556b7ac743004d
NucleonGodX <racerpro41@gmail.com>
2023-10-04 06:43:14 +0530

    routing done

--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello ! xD</div>,
+  },
+  {
+    path: "/about",
+    element: <div>bye xD</div>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-    <App />
+    <RouterProvider router={router}/>
 
 );
 
