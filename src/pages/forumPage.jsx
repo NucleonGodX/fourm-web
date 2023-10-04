48aaf52543106ef8d03adbea65e22578ae2bfb99
NucleonGodX <racerpro41@gmail.com>
2023-10-04 22:37:53 +0530

    state change now working

--- src/pages/forumPage.jsx
+++ src/pages/forumPage.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
+import data from '../forumdata';
 
 export default function ForumPage() {
+  const [heading, setHeading] = useState('');
+  const [content, setContent] = useState('');
 
-    const [heading, setHeading]=React.useState()
-    const [content, setContent]=React.useState()
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newPost = { heading, content };
+    data.push(newPost);
+    setHeading('');
+    setContent('');
+  };
 
   return (
-    <div>forumPage</div>
-  )
+    <form onSubmit={handleSubmit}>
+      <h1>Create Your Forum</h1>
+      <h2>Title</h2>
+      <input
+        type="text"
+        value={heading}
+        onChange={(e) => setHeading(e.target.value)}
+      />
+      <h2>Content</h2>
+      <textarea
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
