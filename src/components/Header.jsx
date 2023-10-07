0181c4e60cce9078ae173f7242b930e06064804e
NucleonGodX <racerpro41@gmail.com>
2023-10-07 09:30:37 +0530

    styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 export default function Header(){
     return(
-        <header className='flex flex-wrap items-center justify-between p-5 bg-black text-cyan-300 text-2xl'>
+        <header className='flex flex-wrap items-center justify-between p-5 bg-black text-white text-2xl'>
         <NavLink to='/' className='car-head'>#YourFourms</NavLink>
         <hr/>
         <div className="flex space-x-8">
--- src/data.jsx
+++ src/data.jsx
@@ -5,16 +5,16 @@ import { Link } from 'react-router-dom'
 
 export default function Data() {
 const [allForums, setAllForums]=React.useState(dataFromLocalStorage)
-    const [myData, setMyData]=React.useState([])
 
     const forumData=allForums
 
-    return(  <>
+    return(  <div className='flex flex-col items-center'>
         {dataFromLocalStorage.map((entry)=>{
-            return( <><h1>{entry.heading}</h1>
-            <p>{entry.content}</p></>)
+            return( <div className=' min-h-[30rem] w-[50rem] bg-red my-20 px-20 py-10 rounded-3xl bg-black flex flex-col inset-x-50 text-cyan-500'>
+                <h1 className='text-4xl py-6 text-yellow-400'>{entry.heading}</h1>
+            <p className='text-2xl'>{entry.content}</p></div>)
         })}
-                <Link to="/forumpage">Create </Link></>
+                <Link to="/forumpage">Create </Link></div>
        ) 
 
     }
