205919f1cd112af14692a7044a3b89287f9bf195
Manit Singh <79140607+NucleonGodX@users.noreply.github.com>
2023-10-10 21:17:24 +0530

    Update forumPage.jsx

--- src/pages/forumPage.jsx
+++ src/pages/forumPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import data from '../forumdata';
+import {useNavigate} from react-router-dom;
 
 export default function ForumPage() {
   const [heading, setHeading] = useState('');
